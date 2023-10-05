ddcf63cb7a947200fe2557efd37c24fd677f0023
iangmenpor <145766951+iangmenpor@users.noreply.github.com>
2023-10-05 16:17:06 +0200

    Inicio del proyecto

---
+++ src/Cpu.java
@@ -0,0 +1,29 @@
+public class Cpu {
+   //Estados, atributos, propiedades
+   public String brand;
+   public String model;
+   public Double cost;
+   // El comando Double son para nª con decimales. pero para numeros enteros se usa Integer
+
+   // Comportamiento o método (funciones)
+   // public(+) método accesible desde cualquier clase
+   // String: tipo devuelto por el método
+   public String getFullName(){
+      return brand + " " + model + " " + cost;
+   // para contatenar usamos la "+" ejem: brand + model + cost
+   // si quieres poner espacios para que no se muestre junto es con " " por medio
+   // las cadenas van siempre con coble comillas "", no es lo mismo sin la comilla. es un numero entero y no sirve para string
+   }
+   public String getBrand(){
+      return brand;
+   }
+   public String getModel(){
+      return model;
+   }
+   public Double getCost(){
+      return cost;
+   }
+
+}
+
+//Esto es la codificación del diagrama Cpu en Lucid.app
