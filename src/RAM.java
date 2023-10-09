4ff7df058fe29834275e2dd4d8d443fbceb619da
iangmenpor <145766951+iangmenpor@users.noreply.github.com>
2023-10-09 17:09:02 +0200

    Añadido dos clases más

--- src/RAM.java
+++ src/RAM.java
@@ -1,2 +1,21 @@
 public class RAM {
+    public String brand;
+    public String capacity;
+    public Double price;
+
+    public String getFullName() {
+        return brand + " " + capacity + " " + price;
+    }
+
+    public String getDeveloper() {
+        return brand;
+    }
+
+    public String getModel() {
+        return capacity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 }
--- src/Tower.java
+++ src/Tower.java
@@ -1,2 +1,21 @@
 public class Tower {
+    public String brand;
+    public String model;
+    public Double price;
+
+    public String getFullName() {
+        return brand + " " + model + " " + price;
+    }
+
+    public String getDeveloper() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 }
